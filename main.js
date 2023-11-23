672a3a8c2ecbfbc98c4a29012683a837b411ee54
Shruti Sen <senshruti2020@gmail.com>
2023-11-23 10:25:25 +0530

    added dark-theme

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    }else{
+        icon.scr = "images/moon.png";
+    }
+}

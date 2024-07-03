aa6ad208904cc80009f52efaa9093ad6aeac23ea
Shruti Sen <senshruti2020@gmail.com>
2024-07-04 00:33:46 +0530

    Add scroll animations

--- main.js
+++ main.js
@@ -38,3 +38,30 @@ projectcloseBtns.forEach((closeBtn) => {
         });
     });
 });
+
+//SCROLL REVEAL ANIMATION 
+//COMMON REVEAL OPTIONS TO CREATE REVEAL ANIMATIONS
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval:200});
+ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.about .professional-list li', {delay: 500, origin: 'right', interval:200});
+ScrollReveal().reveal('.skills-description, .contact-card, .contact-left h2', {delay: 700, origin: 'left'});
+ScrollReveal().reveal('.education, .projects .img-card', {delay: 800, origin: 'bottom' , interval:200});
+ScrollReveal().reveal('.footer-container .group', {delay: 500, origin: 'top'});
+
+
+
+
+
+
+
